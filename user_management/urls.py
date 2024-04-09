1354160e8a540b380991f8f13cd9530e254033d1
Boobalanselvaraj <142650588+Boobalanselvaraj@users.noreply.github.com>
2024-04-09 17:47:47 +0530

    added user-email

--- user_management/urls.py
+++ user_management/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import save_email
 
 urlpatterns = [
-    path('save-email/', save_email, name='save_email'),
+     path('save-email/', save_email, name='save_email'),
 ]
