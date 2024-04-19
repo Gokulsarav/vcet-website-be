7315db635dfc963b00d8ac33d89179bc46baf1e5
Gokulsarav <gokul.slj@gmail.com>
2024-04-19 07:09:49 +0530

    first commit

--- vcetwebsite/settings.py
+++ vcetwebsite/settings.py
@@ -90,6 +90,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mastermail',
+        'USER': 'root',
+        'PASSWORD': 'monster',
+        'HOST': 'localhost',
+        'PORT': 3306,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
